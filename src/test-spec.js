08f7f008de6e61e34d155638504e5869ad98d99a
Lucas Machado <machadolucasvp@gmail.com>
2021-05-09 12:41:17 -0300

    feat: add classical for and reduce test

--- src/test-spec.js
+++ src/test-spec.js
@@ -1,6 +1,16 @@
 module.exports = {
-    seed: [...Array(10000).fill({ id: Math.random() })],
+    seed: [...Array(100000).fill({ id: Math.random() })],
     tests: [
+        {
+            name: 'classical for',
+            fn: (data) => {
+                const copy = []
+                for (let i = 0; i < data.length - 1; i++) {
+                    copy.push(data[i].id)
+                }
+                return copy
+            }
+        },
         {
             name: 'for of',
             fn: (data) => {
@@ -16,6 +26,15 @@ module.exports = {
             fn: (data) => {
                 return data.map(e => e.id)
             }
-        }
+        },
+        {
+            name: 'reduce',
+            fn: (data) => {
+                return data.reduce((acc, e) => {
+                    acc.push(e.id)
+                    return acc
+                }, [])
+            }
+        },
     ]
 }
